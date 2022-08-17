24e88194e0a64c00cf5d9a21a43ef078c943ce96
gareginyesayan <gyesayan@yahoo.com>
2022-08-16 22:44:13 -0400

    Removing -out directory

---
+++ src/LongestPrefix.java
@@ -0,0 +1,47 @@
+/*
+Write a function to find the longest common prefix string amongst an array of strings.
+If there is no common prefix, return an empty string "".
+
+Example 1:
+
+Input: strs = ["flower","flow","flight"]
+Output: "fl"
+Example 2:
+
+Input: strs = ["dog","racecar","car"]
+Output: ""
+Explanation: There is no common prefix among the input strings.
+
+Constraints:
+
+1 <= strs.length <= 200
+0 <= strs[i].length <= 200
+strs[i] consists of only lowercase English letters.
+ */
+
+public class LongestPrefix {
+    public static void main(String[] args) {
+
+        String[] strs = {"flower", "flow", "flight"};
+        String prefix = returnPrefix(strs);
+        System.out.println(prefix);
+    }
+
+    public static String returnPrefix(String[] strs) {
+        String prefix = "";
+        int i = 0;
+        do {
+            for (int j = 0; j < strs.length; j++) {
+                if (i >= strs[j].length()) {
+                    return prefix;
+                }
+                if (strs[j].charAt(i) != strs[0].charAt(i)) {
+                    return prefix;
+                }
+            }
+            prefix = prefix + strs[0].charAt(i);
+            i++;
+        } while (true);
+
+    }
+}
