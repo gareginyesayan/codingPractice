a2bfea819f2bee31e5af990bde5c0a457d8f6ff7
gareginyesayan <gyesayan@yahoo.com>
2022-11-03 20:50:58 -0400

    few other files

--- src/leetcode/SearchInsertPosition.java
+++ src/leetcode/SearchInsertPosition.java
@@ -24,7 +24,7 @@ public class SearchInsertPosition {
 
     public static void main(String[] args) {
         int[] nums = {1,2, 3,5,6};
-        int target = 7;
+        int target = 4;
         System.out.println(searchPosition(nums, target));
     }
 
